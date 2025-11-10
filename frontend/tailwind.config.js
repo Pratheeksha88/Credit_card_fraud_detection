88fe23f6d4d04cca8e61f2b3ff37c6eda14993b7
poorvi <shettypoorvi193@gmail.com>
2025-11-11 00:49:10 +0530

    Enhanced research overview page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -17,5 +17,17 @@ export default {
       },
     },
   },
+  extend: {
+  animation: {
+    'gradient-x': 'gradient-x 8s ease infinite',
+  },
+  keyframes: {
+    'gradient-x': {
+      '0%, 100%': { backgroundPosition: '0% 50%' },
+      '50%': { backgroundPosition: '100% 50%' },
+    },
+  },
+},
+
   plugins: [],
 }
